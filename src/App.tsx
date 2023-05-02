2ba39c83d3fee37566da701064d84a32ac2893c1
sok5188 <sok5188@gmail.com>
2023-05-02 14:20:23 +0900

    addTypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
 function App() {
-  const api = (e) => {
+  const api = () => {
     axios
       .get("http://3.34.232.63:8080/test/stringTest")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log(e.message);
+        console.log(e.code);
+      });
+  };
+  const api2 = () => {
+    axios
+      .post("http://43.201.208.100:8080/auth/sendEmail/sok5188@gmail.com")
       .then((res) => {
         console.log(res);
       })
@@ -25,13 +37,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
         <h2> Homebrew FE </h2>
         <h2> Build... </h2>
-        <button onClick={api}>로그인 없어도 정상</button>
+        <button onClick={api}>홈브루 get test API호출(CORS 설정 필요)</button>
+        <button onClick={api2}>다른 프로젝트 이메일 전송 API</button>
       </header>
     </div>
   );
