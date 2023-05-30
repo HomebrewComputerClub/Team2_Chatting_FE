29be1ded97da22e831f103b707022a5cd2b8d1ff
Hongjin <wad30905@gmail.com>
2023-05-30 17:20:42 +0900

    nginx

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 REPOSITORY=/home/ubuntu/fe
 
 cd $REPOSITORY
-sudo npx pm2 kill
+# sudo npx pm2 kill
 
 # sudo rm -rf node_modules
 
 # sudo yarn install
 
-sudo npx pm2 start yarn --interpreter bash -- start
+# sudo npx pm2 start yarn --interpreter bash -- start
 
 # sudo yarn build
 sudo service nginx restart
