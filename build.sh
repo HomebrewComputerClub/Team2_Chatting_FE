3525402da9d6f30a3da7b78230e67be2b80e9419
kuk329 <kuk329@cau.ac.kr>
2023-05-30 20:33:15 +0900

    build.sh 수정

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ REPOSITORY=/home/ubuntu/fe
 cd $REPOSITORY
 # sudo npx pm2 kill
 
-# sudo rm -rf node_modules
+sudo rm -rf node_modules
 
-# sudo yarn install
+sudo yarn install
 
 # sudo npx pm2 start yarn --interpreter bash -- start
 
-# sudo yarn build
+sudo yarn build
 sudo service nginx restart
